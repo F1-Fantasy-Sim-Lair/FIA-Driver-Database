60bb6890f5958981a399719ad60fedc83bb60bff
Rik <rikthedeveloper@outlook.com>
2024-04-23 14:59:01 +0200

    Add unit tests for scaffolded web api code

---
+++ Web.UnitTests/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Logging.Abstractions;
+using Web.Controllers;
+
+namespace Web.UnitTests.Controllers;
+internal class WeatherForecastControllerTests
+{
+    [Test]
+    public void Get_ReturnsNext5DaysOfForecast()
+    {
+        var sut = new WeatherForecastController(new NullLogger<WeatherForecastController>());
+        var forecasts = sut.Get();
+        var today = DateOnly.FromDateTime(DateTime.Today);
+        forecasts.Should().HaveCount(5)
+            .And.AllSatisfy(fc => fc.Date.Should().BeAfter(today));
+    }
+}
