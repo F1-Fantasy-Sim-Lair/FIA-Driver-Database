00c131545dc003728d8c8c852e662f43ba3e84ed
Rik <rikthedeveloper@outlook.com>
2024-04-25 21:02:11 +0200

    Create Snowflake struct for database IDs

---
+++ Web.UnitTests/Types/SnowflakeTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Web.Types;
+
+namespace Web.UnitTests.Types;
+internal class SnowflakeTests
+{
+    [Test]
+    public void Snowflake_CanBeCreated()
+    {
+        var _ = new Snowflake(0);
+    }
+
+    [Test]
+    public void Snowflake__TryParse_CanParseFromString()
+    {
+        Snowflake.TryParse("1233076034211942421", out var value).Should().BeTrue();
+        value.Value.Should().Be(1233076034211942421);
+    }
+
+    [Test]
+    [TestCase("12330760342119424217")] // Value is 1 character too long
+    [TestCase("123307603421194242A")] // Value contains non-numeric characters
+    public void Snowflake_TryParse_ReturnsFalseForInvalidValue(string strValue)
+    {
+        Snowflake.TryParse(strValue, out var value).Should().BeFalse();
+        value.Value.Should().Be(0);
+    }
+}
---
+++ Web/Types/Snowflake.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Web.Types;
+
+public readonly record struct Snowflake(long Value) : IEquatable<Snowflake>, IParsable<Snowflake>
+{
+    public override int GetHashCode()
+        => Value.GetHashCode();
+
+    public override string ToString()
+        => Value.ToString();
+
+    public static implicit operator Snowflake(long value) => new(value);
+
+    public static Snowflake Parse(string s) => Parse(s, null);
+    public static Snowflake Parse(string s, IFormatProvider? provider) => long.Parse(s, provider);
+
+    public static bool TryParse(string s, out Snowflake @out) => TryParse(s, null, out @out);
+    public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Snowflake @out)
+    {
+        if (long.TryParse(s, provider, out var result))
+        {
+            @out = new(result);
+            return true;
+        }
+
+        @out = default;
+        return false;
+    }
+}
