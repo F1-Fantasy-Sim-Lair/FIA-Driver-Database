4c7021f350247d5b581c50fce65b3a82d51171a7
Rik <rikthedeveloper@outlook.com>
2024-04-25 21:02:50 +0200

    Configure Entity Framework Snowflake conversion

--- Web/Model/EF/DriverDatabaseContext.cs
+++ Web/Model/EF/DriverDatabaseContext.cs
@@ -1,7 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Web.Types;
 
 namespace Web.Model.EF;
 
 public class DriverDatabaseContext(DbContextOptions options) : DbContext(options)
 {
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.Properties<Snowflake>().HaveConversion<SnowflakeConverter>();
+    }
+
+    public class SnowflakeConverter() : ValueConverter<Snowflake, long>(v => v.Value, v => new(v));
 }
