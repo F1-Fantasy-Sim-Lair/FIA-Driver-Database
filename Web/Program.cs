29e87c3d594b0af618a61790972c847af244dc6c
Rik <rikthedeveloper@outlook.com>
2024-11-26 13:12:13 +0100

    Add + configure IdGen package

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,8 +1,13 @@
+using IdGen;
+using IdGen.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Web.Model.EF;
 using Web.Model.Repository;
+using Web.Types;
 
 namespace Web;
+
+public delegate Snowflake GenerateId();
 
 public class Program
 {
@@ -13,6 +18,8 @@ public class Program
         builder.Services.AddScoped<IUnitOfWork, DefaultUnitOfWork>();
         builder.Services.AddDbContext<DriverDatabaseContext>(options =>
             options.UseSqlite(builder.Configuration.GetConnectionString("DriverDatabaseContext") ?? throw new InvalidOperationException("Connection string 'DriverDatabaseContext' not found.")));
+        builder.Services.AddIdGen(1);
+        builder.Services.AddSingleton<GenerateId>(sp => () => new(sp.GetRequiredService<IIdGenerator<long>>().CreateId()));
 
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
