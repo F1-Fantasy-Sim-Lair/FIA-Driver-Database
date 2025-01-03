8f65bb7a852f955f21c3eb338dcef6e39d817bb2
Rik <rikthedeveloper@outlook.com>
2025-01-03 21:42:35 +0100

    Add stub Teams controller

---
+++ Web.IntegrationTests/TeamsTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Web.IntegrationTests.Common;
+
+namespace Web.IntegrationTests;
+public class TeamsTests
+{
+    [Test]
+    public async Task GetAllTeams_WithoutAuthorization_Returns401Unauthorized()
+    {
+        // Arrange
+        using var client = new WebApplicationFactory().CreateClient();
+        using var request = new HttpRequestMessage(HttpMethod.Get, "teams");
+
+        // Act
+        using var response = await client.SendAsync(request);
+
+        // Assert
+        response.Should().Be401Unauthorized();
+    }
+
+    [Test]
+    public async Task GetAllTeams_Returns200OK()
+    {
+        // Arrange
+        using var factory = new WebApplicationFactory();
+        using var client = factory.CreateAuthenticatedClient(await factory.TestUsers.GetPrincipal("testuser"));
+        using var request = new HttpRequestMessage(HttpMethod.Get, "teams");
+
+        // Act
+        using var response = await client.SendAsync(request);
+
+        // Assert
+        response.Should().BeSuccessful();
+    }
+}
---
+++ Web/Controllers/TeamsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+
+[Route("teams")]
+public class TeamsController : ControllerBase
+{
+    [HttpGet, Authorize]
+    public async Task<IActionResult> GetAllTeams()
+    {
+        return Ok();
+    }
+}
