7f0902c479cb75a70b9cf87d5486913bb8c9ba57
Rik <rikthedeveloper@outlook.com>
2025-01-03 21:41:58 +0100

    Add helper code to test authenticated endpoints

---
+++ Web.IntegrationTests/Common/TestUserManager.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System.Security.Claims;
+
+namespace Web.IntegrationTests.Common;
+
+internal class TestUserManager(WebApplicationFactory webApplicationFactory, IServiceProvider serviceProvider)
+{
+    readonly WebApplicationFactory webApplicationFactory = webApplicationFactory;
+    readonly IServiceProvider serviceProvider = serviceProvider;
+
+    public async Task<IdentityUser> CreateUser(string username)
+    {
+        using var serviceScope = serviceProvider.CreateScope();
+        var userManager = serviceScope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+        var currentUser = new IdentityUser();
+        await userManager.SetUserNameAsync(currentUser, username);
+        await userManager.CreateAsync(currentUser);
+        return currentUser;
+    }
+
+    public async Task<ClaimsPrincipal> GetPrincipal(IdentityUser user)
+    {
+        using var serviceScope = serviceProvider.CreateScope();
+        return await GetPrincipal(user, serviceScope.ServiceProvider);
+    }
+
+    public async Task<ClaimsPrincipal> GetPrincipal(string username)
+    {
+        using var serviceScope = serviceProvider.CreateScope();
+        var userManager = serviceScope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+        return await GetPrincipal(await userManager.FindByNameAsync(username) ?? new IdentityUser("testuser"), serviceScope.ServiceProvider);
+    }
+
+    static async Task<ClaimsPrincipal> GetPrincipal(IdentityUser user, IServiceProvider serviceProvider)
+    {
+        var claimsPrincipalFactory = serviceProvider.GetRequiredService<IUserClaimsPrincipalFactory<IdentityUser>>();
+        return await claimsPrincipalFactory.CreateAsync(user);
+    }
+}
--- Web.IntegrationTests/Common/WebApplicationFactory.cs
+++ Web.IntegrationTests/Common/WebApplicationFactory.cs
@@ -1,13 +1,28 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.Mvc.Testing.Handlers;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Net.Http.Headers;
+using System.Net;
+using System.Security.Claims;
 using Web.Model.EF;
 
 namespace Web.IntegrationTests.Common;
 internal class WebApplicationFactory : WebApplicationFactory<Program>
 {
+    public WebApplicationFactory() : base()
+    {
+        TestUsers = new TestUserManager(this, Services);
+    }
+
+    public TestUserManager TestUsers { get; }
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         var configDict = new Dictionary<string, string?>
@@ -25,5 +40,61 @@ internal class WebApplicationFactory : WebApplicationFactory<Program>
         scope.ServiceProvider.GetRequiredService<DriverDatabaseContext>().Database.EnsureDeleted();
         scope.ServiceProvider.GetRequiredService<DriverDatabaseContext>().Database.EnsureCreated();
         return host;
+    }
+
+    public HttpClient CreateAuthenticatedClient(ClaimsPrincipal principal)
+        => CreateAuthenticatedClient(principal, new());
+
+    public HttpClient CreateAuthenticatedClient(ClaimsPrincipal principal, WebApplicationFactoryClientOptions clientOptions)
+    {
+        using var serviceScope = Services.CreateScope();
+        var authProperties = new AuthenticationProperties()
+        {
+            ExpiresUtc = DateTimeOffset.Now.AddDays(1),
+            IssuedUtc = DateTimeOffset.Now,
+            Items =
+            {
+                new KeyValuePair<string, string?>(".AuthScheme", "Discord"),
+                new KeyValuePair<string, string?>("LoginProvider", "Discord"),
+                new KeyValuePair<string, string?>(".Token.access_token", "abcdefghijklmnopqrstuvwxyz"),
+                new KeyValuePair<string, string?>(".Token.refresh_token", "1234567890"),
+                new KeyValuePair<string, string?>(".Token.token_type", "Bearer"),
+                new KeyValuePair<string, string?>(".TokenNames", "access_token;refresh_token;token_type;expires_at"),
+                new KeyValuePair<string, string?>(".Token.expires_at", DateTimeOffset.Now.AddDays(1).ToString("O"))
+            }
+        };
+
+        var cookieValueBytes = serviceScope.ServiceProvider.GetRequiredService<IDataProtectionProvider>()
+            .CreateProtector("Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationMiddleware", IdentityConstants.ApplicationScheme, "v2")
+            .Protect(TicketSerializer.Default.Serialize(new AuthenticationTicket(principal, authProperties, IdentityConstants.ApplicationScheme)));
+
+        var cookieName = CookieAuthenticationDefaults.CookiePrefix + Uri.EscapeDataString(IdentityConstants.ApplicationScheme);
+        var cookieValue = Base64UrlTextEncoder.Encode(cookieValueBytes);
+
+        var cookieContainer = new CookieContainer();
+
+        var setCookieHeader = new SetCookieHeaderValue(cookieName, cookieValue)
+        {
+            Path = "/",
+            HttpOnly = true,
+            Secure = false,
+            Expires = DateTimeOffset.Now.AddDays(1),
+            Domain = clientOptions.BaseAddress.Host,
+            SameSite = SameSiteMode.None
+        };
+
+        cookieContainer.SetCookies(clientOptions.BaseAddress, setCookieHeader.ToString());
+
+        return CreateDefaultClient(clientOptions.BaseAddress, CreateHandlersCore().ToArray());
+
+        IEnumerable<DelegatingHandler> CreateHandlersCore()
+        {
+            if (clientOptions.AllowAutoRedirect)
+            {
+                yield return new RedirectHandler(clientOptions.MaxAutomaticRedirections);
+            }
+
+            yield return new CookieContainerHandler(cookieContainer);
+        }
     }
 }
