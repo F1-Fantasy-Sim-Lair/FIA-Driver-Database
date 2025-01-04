75ec3e8d9254ddaa881ed2c84618daf866e10c7b
Rik <rikthedeveloper@outlook.com>
2025-01-04 14:41:51 +0100

    Get TeamsController data from database

---
+++ Web.UnitTests/Controllers/TeamsControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Web.Controllers;
+using Web.Model;
+using Web.UnitTests.Fakes;
+
+namespace Web.UnitTests.Controllers;
+internal class TeamsControllerTests
+{
+    [Test]
+    public async Task GetAllTeams_ReturnsAllTeams()
+    {
+        // Arrange
+        var uow = new FakeUnitOfWork();
+        uow.Repository<Team>().AddRange([
+            new Team(1) { Name = "Team 1" },
+            new Team(2) { Name = "Team 1" },
+            new Team(3) { Name = "Team 1" }]);
+        await uow.CompleteAsync();
+
+        var controller = new TeamsController(uow);
+        // Act
+        var result = await controller.GetAllTeams();
+        // Assert
+        result.Should().BeOfType<OkObjectResult>()
+            .Which.Value.Should().NotBeNull()
+            .And.BeAssignableTo<IEnumerable<TeamResponse>>()
+            .Which.Should().HaveCount(3);
+    }
+}
--- Web/Controllers/TeamsController.cs
+++ Web/Controllers/TeamsController.cs
@@ -1,14 +1,22 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Web.Model;
+using Web.Model.Repository;
+using Web.Types;
+using Web.Utils;
 
 namespace Web.Controllers;
 
+public record class TeamResponse(Snowflake Id, string Name);
+
 [Route("teams")]
-public class TeamsController : ControllerBase
+public class TeamsController(IUnitOfWork unitOfWork) : ControllerBase
 {
+    private readonly IUnitOfWork unitOfWork = unitOfWork;
+
     [HttpGet, Authorize]
     public async Task<IActionResult> GetAllTeams()
     {
-        return Ok();
+        return Ok(await unitOfWork.Repository<Team>().Query().Select(t => new TeamResponse(t.TeamId, t.Name)).ToListAsync());
     }
 }
