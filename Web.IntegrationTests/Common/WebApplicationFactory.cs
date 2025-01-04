01b515d6d17d46bf9419e3c19a79fdea071af5ba
Rik <rikthedeveloper@outlook.com>
2025-01-04 17:49:58 +0100

    Fix a timing issue with setting up the test database
    When deleting the database after the Host was created, backgroundservices were already started, causing data loss. Just moving the existing code above CreateHost causes a stack overflow, because calling Services creates the Host internally. The DbContext instead needs to be manually created.

--- Web.IntegrationTests/Common/WebApplicationFactory.cs
+++ Web.IntegrationTests/Common/WebApplicationFactory.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.Mvc.Testing.Handlers;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -16,6 +17,8 @@ using Web.Model.EF;
 namespace Web.IntegrationTests.Common;
 internal class WebApplicationFactory : WebApplicationFactory<Program>
 {
+    const string TestConnectionString = "Data Source=fiadriverdatabase.db";
+
     public WebApplicationFactory() : base()
     {
         TestUsers = new TestUserManager(this, Services);
@@ -27,7 +30,7 @@ internal class WebApplicationFactory : WebApplicationFactory<Program>
     {
         var configDict = new Dictionary<string, string?>
         {
-            { "ConnectionStrings:DriverDatabaseContext", "Data Source=fiadriverdatabase.db" },
+            { "ConnectionStrings:DriverDatabaseContext", TestConnectionString },
             { "Database:MigrateOnStartup", "false" }
         };
         builder.ConfigureAppConfiguration(config => config.AddInMemoryCollection(configDict));
@@ -35,11 +38,12 @@ internal class WebApplicationFactory : WebApplicationFactory<Program>
 
     protected override IHost CreateHost(IHostBuilder builder)
     {
-        var host = base.CreateHost(builder);
-        using var scope = host.Services.CreateScope();
-        scope.ServiceProvider.GetRequiredService<DriverDatabaseContext>().Database.EnsureDeleted();
-        scope.ServiceProvider.GetRequiredService<DriverDatabaseContext>().Database.EnsureCreated();
-        return host;
+        using var dbContext = new DriverDatabaseContext(new DbContextOptionsBuilder<DriverDatabaseContext>()
+            .UseSqlite(TestConnectionString)
+            .Options);
+        dbContext.Database.EnsureDeleted();
+        dbContext.Database.EnsureCreated();
+        return base.CreateHost(builder);
     }
 
     public HttpClient CreateAuthenticatedClient(ClaimsPrincipal principal)
