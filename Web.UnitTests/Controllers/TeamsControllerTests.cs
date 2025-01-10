cdd37759072ada0380c7134e6d682fc6469bf090
Rik <rikthedeveloper@outlook.com>
2025-01-10 13:36:20 +0100

    Add missing CRU methods for teams

--- Web.UnitTests/Controllers/TeamsControllerTests.cs
+++ Web.UnitTests/Controllers/TeamsControllerTests.cs
@@ -19,12 +19,71 @@ internal class TeamsControllerTests
         await uow.CompleteAsync();
 
         var controller = new TeamsController(uow);
+
         // Act
         var result = await controller.GetAllTeams();
+
         // Assert
         result.Should().BeOfType<OkObjectResult>()
             .Which.Value.Should().NotBeNull()
             .And.BeAssignableTo<IEnumerable<TeamResponse>>()
             .Which.Should().HaveCount(3);
+    }
+
+    [Test]
+    public async Task GetTeam_ReturnsTeam()
+    {
+        // Arrange
+        var uow = new FakeUnitOfWork();
+        var team = new Team(1) { Name = "Team 1" };
+        uow.Repository<Team>().Add(team);
+        await uow.CompleteAsync();
+        var controller = new TeamsController(uow);
+
+        // Act
+        var result = await controller.GetTeam(1);
+
+        // Assert
+        var teamResp = result.Should().BeOfType<OkObjectResult>()
+            .Which.Value.Should().NotBeNull()
+            .And.BeOfType<TeamResponse>().Subject;
+        teamResp.Name.Should().Be("Team 1");
+    }
+
+    [Test]
+    public async Task CreateTeam_ReturnsCreatedTeam()
+    {
+        // Arrange
+        var uow = new FakeUnitOfWork();
+        var controller = new TeamsController(uow);
+
+        // Act
+        var result = await controller.CreateTeam(new CreateTeamRequest("Team 1"), () => 1);
+
+        // Assert
+        var teamResp = result.Should().BeOfType<CreatedAtActionResult>()
+            .Which.Value.Should().NotBeNull()
+            .And.BeOfType<TeamResponse>().Subject;
+        teamResp.Name.Should().Be("Team 1");
+    }
+
+    [Test]
+    public async Task UpdateTeam_ReturnsUpdatedTeam()
+    {
+        // Arrange
+        var uow = new FakeUnitOfWork();
+        var team = new Team(1) { Name = "Team 1" };
+        uow.Repository<Team>().Add(team);
+        await uow.CompleteAsync();
+        var controller = new TeamsController(uow);
+
+        // Act
+        var result = await controller.UpdateTeam(1, new CreateTeamRequest("Team 2"));
+
+        // Assert
+        var teamResp = result.Should().BeOfType<OkObjectResult>()
+            .Which.Value.Should().NotBeNull()
+            .And.BeOfType<TeamResponse>().Subject;
+        teamResp.Name.Should().Be("Team 2");
     }
 }
--- Web/Controllers/TeamsController.cs
+++ Web/Controllers/TeamsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Web.Authorization;
 using Web.Model;
 using Web.Model.Repository;
 using Web.Types;
@@ -7,16 +8,54 @@ using Web.Utils;
 
 namespace Web.Controllers;
 
-public record class TeamResponse(Snowflake Id, string Name);
+public record class TeamResponse(Snowflake Id, string Name)
+{
+    public TeamResponse(Team team) : this(team.TeamId, team.Name) { }
+}
+public record class CreateTeamRequest(string Name);
 
 [Route("teams")]
 public class TeamsController(IUnitOfWork unitOfWork) : ControllerBase
 {
     private readonly IUnitOfWork unitOfWork = unitOfWork;
 
-    [HttpGet, Authorize(Policy = "IsDirector")]
+    [HttpGet]
     public async Task<IActionResult> GetAllTeams()
     {
-        return Ok(await unitOfWork.Repository<Team>().Query().Select(t => new TeamResponse(t.TeamId, t.Name)).ToListAsync());
+        return Ok(await unitOfWork.Repository<Team>().Query().Select(t => new TeamResponse(t)).ToListAsync());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetTeam(Snowflake id)
+    {
+        var team = await unitOfWork.Repository<Team>().GetByIdAsync(id);
+        if (team is null)
+            return NotFound();
+
+        return Ok(new TeamResponse(team.TeamId, team.Name));
+    }
+
+    [HttpPost, Authorize(Policies.IsDirector)]
+    public async Task<IActionResult> CreateTeam([FromBody] CreateTeamRequest createTeamRequest, [FromServices] GenerateId generateId)
+    {
+        var team = new Team(generateId())
+        {
+            Name = createTeamRequest.Name
+        };
+        unitOfWork.Repository<Team>().Add(team);
+        await unitOfWork.CompleteAsync();
+        return CreatedAtAction(nameof(GetTeam), new { id = team.TeamId }, new TeamResponse(team));
+    }
+
+    [HttpPut("{id}"), Authorize(Policies.IsDirector)]
+    public async Task<IActionResult> UpdateTeam(Snowflake id, [FromBody] CreateTeamRequest createTeamRequest)
+    {
+        var team = await unitOfWork.Repository<Team>().GetByIdAsync(id);
+        if (team is null)
+            return NotFound();
+
+        team.Name = createTeamRequest.Name;
+        await unitOfWork.CompleteAsync();
+        return Ok(new TeamResponse(team));
     }
 }
